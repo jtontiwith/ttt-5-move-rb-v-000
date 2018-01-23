369405f098b5ebea35dfa82c9d0a85e31fc6981a
Jon Tonti <jtonti@gmail.com>
2018-01-23 18:20:56 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -17,9 +17,9 @@ end
 
 def input_to_index(p_input)
   index = p_input.to_i
-  puts index + 2
-  #board = [" "," "," "," "," "," "," "," "," "]
-  #move(board, index)
+  puts index
+  board = [" "," "," "," "," "," "," "," "," "]
+  move(board, index)
 end
 
 def move(b_array, pmove, p_char = "X")
